babe46d344b2661d369b21e7bc2251c02177d694
mohasinac <mohsin.ahamed@hotmail.com>
2020-11-05 01:06:06 +0530

    login

--- hello-next/pages/api/hello.ts
+++ hello-next/pages/api/hello.ts
@@ -7,6 +7,10 @@ import { NextApiRequest, NextApiResponse } from "next"
 //inside pages only components
 // inside /api only apis and no components
 export default function (req: NextApiRequest, res: NextApiResponse) {
+  console.log('requestbody' ,req.body)
+  //console.log('response', res.sendDate)
+  res.setHeader('X-custom','my header');
+  res.setHeader('Set-Cookie','mycookie=true');
   res.statusCode = 200
   res.json({ number: Math.floor(Math.random() * 10) })
 }
---
+++ hello-next/pages/login/index.tsx
@@ -0,0 +1,11 @@
+export default function (){
+
+    return (
+        <form >
+        <input type="text" value="admin" />
+            <input type="password" value="admin" />
+            <br />
+            <input type="submit" value="Login" />
+        </form>
+    );
+}
