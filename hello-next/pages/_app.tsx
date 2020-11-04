b87047f8864d9b8b78f17604d82590c5236368bf
mohasinac <mohsin.ahamed@hotmail.com>
2020-11-04 23:55:58 +0530

    file _app.tsx learnt

--- hello-next/pages/_app.tsx
+++ hello-next/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 
+//responsible for rendering pages
+// component and props passed to the component internally
+// both server and client
 function MyApp({ Component, pageProps }) {
+  //console.log('Hello from app');
   return <Component {...pageProps} />
 }
 
--- hello-next/pages/_document.tsx
+++ hello-next/pages/_document.tsx
@@ -1,7 +1,9 @@
-import Document , { Html ,Head ,Main ,NextScript   } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+//Use file as minimum as possible only for template or meta tags which are fixed
+//only executed on server
 export default class CustomDocument extends Document {
-    render(){
+    render() {
         return (
             <Html>
                 <Head>
